4022d78fb8a35ef59b9db9710de23c7a43bd30e3
Naman-Arora <namanarora166@gmail.com>
2022-06-02 14:14:56 -0500

    Completed January 26

---
+++ completed/january26/ValuesOFIJK.java
@@ -0,0 +1,17 @@
+package completed.january26;
+public class ValuesOFIJK {
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 3;
+        int k = 4;
+        int x = i + j + k;
+
+        i = (x - i - j);
+        j = (x - j - k);
+        k = (x - i - k);
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+    }
+}
